e7e7672c3a607d0b0154ae16265036146f9dc55f
renuk96 <116861391+renuk96@users.noreply.github.com>
2023-03-28 02:17:24 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world and welcome to jenkins training: updated using scm poll"); 
+        System.out.println("Hello world and welcome to jenkins training: updated using scm polls"); 
     }
 }
