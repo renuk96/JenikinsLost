3d52f65733783a8711cbac698b28c28d4d87fd7d
renuk96 <116861391+renuk96@users.noreply.github.com>
2023-03-28 01:50:01 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world and welcome to jenkins training"); 
+        System.out.println("Hello world and welcome to jenkins training: updated using webhooks"); 
     }
 }
